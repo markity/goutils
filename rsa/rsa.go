c98025ae1a638479ac0b4f12d2bd7311b5d02c0e
Markity <3402002560@qq.com>
2020-02-03 07:46:36 +0000

    修改变量名

--- rsa/rsa.go
+++ rsa/rsa.go
@@ -13,7 +13,7 @@ import (
 	"os"
 )
 
-func GenerateRsaKeyToFile(bits int, privateKeyPath string, publicKeyPath string) (err error) {
+func GenerateRSAKeyToFile(bits int, privateKeyPath string, publicKeyPath string) (err error) {
 	pri, err := rsa.GenerateKey(rand.Reader, bits)
 	if err != nil {
 		err = errors.New(fmt.Sprintf("failed to generate private key: %v", err))
@@ -57,7 +57,7 @@ func GenerateRsaKeyToFile(bits int, privateKeyPath string, publicKeyPath string)
 	return nil
 }
 
-// LoadPKCS1PrivateKeyFromFile loads a rsa private key from a file
+// LoadPKCS1PrivateKeyFromFile loads a RSA private key from a file
 func LoadPKCS1PrivateKeyFromFile(filePath string) (*rrsa.PrivateKey, error) {
 	fileBytes, err := ioutil.ReadFile(filePath)
 	if err != nil {
@@ -72,7 +72,7 @@ func LoadPKCS1PrivateKeyFromFile(filePath string) (*rrsa.PrivateKey, error) {
 	return x509.ParsePKCS1PrivateKey(block.Bytes)
 }
 
-// LoadPublicKeyFromFile loads a rsa public key from a file
+// LoadPublicKeyFromFile loads a RSA public key from a file
 func LoadPublicKeyFromFile(filePath string) (*rrsa.PublicKey, error) {
 	fileBytes, err := ioutil.ReadFile(filePath)
 	if err != nil {
@@ -97,25 +97,25 @@ func LoadPublicKeyFromFile(filePath string) (*rrsa.PublicKey, error) {
 	return pubKey, nil
 }
 
-// RsaEncrypt encrypts clear text by using rsa public key, and returns the cipher text
-func RsaEncrypt(publicKey *rrsa.PublicKey, clearText []byte) ([]byte, error) {
+// RSAEncrypt encrypts clear text by using rsa public key, and returns the cipher text
+func RSAEncrypt(publicKey *rrsa.PublicKey, clearText []byte) ([]byte, error) {
 	return rrsa.EncryptPKCS1v15(rand.Reader, publicKey, clearText)
 }
 
-// RsaDecrypt decrypts cipher text by using rsa private key, and returns the clear text
-func RsaDecrypt(privateKey *rrsa.PrivateKey, cipherText []byte) ([]byte, error) {
+// RSADecrypt decrypts cipher text by using rsa private key, and returns the clear text
+func RSADecrypt(privateKey *rrsa.PrivateKey, cipherText []byte) ([]byte, error) {
 	return rrsa.DecryptPKCS1v15(rand.Reader, privateKey, cipherText)
 }
 
-// RsaSign signs clear text by using rsa private key, and returns the signature
-func RsaSign(privateKey *rrsa.PrivateKey, hash crypto.Hash, clearText []byte) ([]byte, error) {
+// RSASign signs clear text by using rsa private key, and returns the signature
+func RSASign(privateKey *rrsa.PrivateKey, hash crypto.Hash, clearText []byte) ([]byte, error) {
 	h := hash.New()
 	h.Write(clearText)
 	return rsa.SignPKCS1v15(rand.Reader, privateKey, hash, h.Sum(nil))
 }
 
-// RsaVerifySign verifies signature by using public key and clear text
-func RsaVerifySign(publicKey *rrsa.PublicKey, hash crypto.Hash, signature []byte, clearText []byte) bool {
+// RSAVerifySignature verifies signature by using public key and clear text
+func RSAVerifySignature(publicKey *rrsa.PublicKey, hash crypto.Hash, signature []byte, clearText []byte) bool {
 	h := hash.New()
 	h.Write(clearText)
 	if err := rsa.VerifyPKCS1v15(publicKey, hash, h.Sum(nil), signature); err != nil {
--- tests/rsa/rsa_test.go
+++ tests/rsa/rsa_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func TestAll(t *testing.T) {
-	if err := rsa.GenerateRsaKeyToFile(2048, "app_private_key.pem", "app_public_key.pem"); err != nil {
+	if err := rsa.GenerateRSAKeyToFile(2048, "app_private_key.pem", "app_public_key.pem"); err != nil {
 		t.Fatal(err)
 	}
 
@@ -26,23 +26,23 @@ func TestAll(t *testing.T) {
 
 	clearText := []byte("神秘的密文, it's very secert")
 
-	cipher, err := rsa.RsaEncrypt(pub, clearText)
+	cipher, err := rsa.RSAEncrypt(pub, clearText)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if clearText2, err := rsa.RsaDecrypt(pri, cipher); err != nil {
+	if clearText2, err := rsa.RSADecrypt(pri, cipher); err != nil {
 		t.Fatal(err)
 	} else if string(clearText) != string(clearText2) {
 		t.Fatal(errors.New(fmt.Sprintf("text does not match: '%s' and '%s'", clearText, clearText2)))
 	}
 
-	signature, err := rsa.RsaSign(pri, crypto.SHA256, clearText)
+	signature, err := rsa.RSASign(pri, crypto.SHA256, clearText)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if !rsa.RsaVerifySign(pub, crypto.SHA256, signature, clearText) {
+	if !rsa.RSAVerifySignature(pub, crypto.SHA256, signature, clearText) {
 		t.Fatal(errors.New("verify sign failed"))
 	}
 }
